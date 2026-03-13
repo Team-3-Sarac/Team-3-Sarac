9b446f2ef9f985f859bf68eaf7250feaf52a1018
Inaaya Rana <rabiakashif2000@gmail.com>
2026-03-12 22:08:18 -0500

    frontend/backend connection updates

--- frontend/app/types/event.ts
+++
@@ -1,9 +0,0 @@
-// dashboard - key events
-
-import { EventType } from "./enums";
-
-export interface MatchEvent {
-    event_type: EventType;
-    description: string;
-    created_at: string; // will calculate time from this
-}
