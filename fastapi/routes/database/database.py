6fe348df1f03cf2bfe75e34bc1fe4ace686390a2
Pam <68263385+3em07@users.noreply.github.com>
2026-04-20 10:13:59 -0500

    minor fix:  league and team extraction in video ingestion; ensured rule based matching is applied correctly with the teams we have stored in our db

--- fastapi/routes/database/database.py
+++ fastapi/routes/database/database.py
@@ -12,9 +12,13 @@ mongo_root_pass = os.getenv("MONGO_ROOT_PASSWORD", "")
 mongo_name = os.getenv("MONGO_DATABASE", "test")
 mongo_host = os.getenv("MONGO_HOST", "localhost")
 
-client = AsyncIOMotorClient(
-    f"mongodb://{quote_plus(mongo_root)}:{quote_plus(mongo_root_pass)}@{mongo_host}:27017/admin"
-)
+# mongo uri 
+if mongo_root and mongo_root_pass:
+    uri = f"mongodb://{quote_plus(mongo_root)}:{quote_plus(mongo_root_pass)}@{mongo_host}:27017/admin"
+else:
+    uri = f"mongodb://{mongo_host}:27017"
+# creating client
+client = AsyncIOMotorClient(uri)
 
 db = client[mongo_name]
 
